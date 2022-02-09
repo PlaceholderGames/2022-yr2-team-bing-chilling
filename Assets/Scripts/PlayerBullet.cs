7eab56d20e0c9fd5f718f096c19333c0519cd390
scrab333 <72270337+scrab333@users.noreply.github.com>
2022-02-09 20:51:30 +0000

    improved a bullet a bit and added dummy enemy

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerBullet : MonoBehaviour
 {
     //making an instance if we want to for example upgrade the damage on bullet
-    public static PlayerBullet instance;
+    //public static PlayerBullet instance;
 
     //damage that the bullet will do
-    public float damageToDo;
+    public float damageToDoForBlue;
+    public float damageToDoForRed;
+
+
+    public bool thisIsARedBullet;
+    public bool thisIsABlueBullet;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +30,21 @@ public class PlayerBullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+
+        if (collision.gameObject.tag == "EnemyRed" && thisIsABlueBullet == true)
+        {
+            Debug.Log("BING CHILLING red is dead");
+
+        }
+
+        if (collision.gameObject.tag == "EnemyBlue" && thisIsARedBullet == true)
+        {
+            Debug.Log("BING CHILLING blue is dead");
+
+        }
+
         //destroy it's self on contact with other object
         Destroy(gameObject);
-
     }
 
 }
