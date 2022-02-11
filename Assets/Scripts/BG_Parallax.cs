33dcf551717a8caa9a884079f7f05b693f2ee03a
DinoPT <dinarte85@gmail.com>
2022-02-11 10:24:40 +0000

    BG_Parallax Script finished

--- Assets/Scripts/BG_Parallax.cs
+++ Assets/Scripts/BG_Parallax.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BG_Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2 StartPos;
+    [SerializeField] int moveModifier;
+
+    private void Start()
     {
-        
+        StartPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 pz = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        float posX = Mathf.Lerp(transform.position.x, StartPos.x + (pz.x * moveModifier), 2f * Time.deltaTime);
+        float posY = Mathf.Lerp(transform.position.y, StartPos.y + (pz.y * moveModifier), 2f * Time.deltaTime);
+
+        transform.position = new Vector3(posX, posY, 0);    
     }
 }
