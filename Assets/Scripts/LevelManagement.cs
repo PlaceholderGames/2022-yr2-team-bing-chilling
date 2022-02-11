56d3728cbabf4129276908e53e3b72364d28be49
DinoPT <dinarte85@gmail.com>
2022-02-11 12:26:36 +0000

    LevelManagement Script
    Containing NextLevel() function to transiction from the menu to the levels, as well from level to level

---
+++ Assets/Scripts/LevelManagement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManagement : MonoBehaviour
+{
+    // To be used on the "PLAY" button to move from the menu to a level.
+    // To use on the finish of the level to load the next one.
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
