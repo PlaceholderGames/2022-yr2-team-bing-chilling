fbbc8e6db3c3e76aa3c356380fbe77eb4de02d61
Luckyxzx <alanlee2001369@gmail.com>
2022-02-17 11:35:44 +0000

    Fixed
    Fixed because of Albert Einstein

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,11 +22,13 @@ public class Player : MonoBehaviour
     public HealthBar healthBar;
 
 
+
     // Start is called before the first frame update
     void Start()
     {
+
         currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+       healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
