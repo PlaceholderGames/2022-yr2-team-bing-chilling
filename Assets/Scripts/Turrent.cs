cb131490bc616516b3c4f8dcbb534a0ce7632984
scrab333 <72270337+scrab333@users.noreply.github.com>
2022-02-19 00:12:27 +0000

    Placed turrnents + it follows enemies

--- Assets/Scripts/Turrent.cs
+++ Assets/Scripts/Turrent.cs
@@ -4,19 +4,59 @@ using UnityEngine;
 
 public class Turrent : MonoBehaviour
 {
-    public Transform target;
+    private Transform target;
     public float rangeOfTurrent = 15f;
+
+    public Transform partToRotate;
+
+    //REMEMBER TO ADD A SECOND TAG ON ENEMIES
+    public string enemyTag = "Enemy";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= rangeOfTurrent)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( target == null)
+            return;
+
+        Vector3 direction = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = lookRotation.eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
     }
 
     private void OnDrawGizmosSelected()
