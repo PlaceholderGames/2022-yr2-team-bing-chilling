b5412d7dbcadeb277deca553eadc54653ca5071c
scrab333 <72270337+scrab333@users.noreply.github.com>
2022-02-24 16:53:01 +0000

    The enemy now follows the player

--- Assets/Scripts/EnemyFPS.cs
+++ Assets/Scripts/EnemyFPS.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+
 
 public class EnemyFPS : MonoBehaviour
 {
@@ -9,23 +11,61 @@ public class EnemyFPS : MonoBehaviour
     public GameObject Player;
     public float MobDistanceRun = 4.4f;
 
+    //which am taking from Ka
+    //Code from Brackeys on YouTube
+    public float starthealth = 100;
+    private float health;
+    public Image healthBar;
+
     private void Start()
     {
+        health = starthealth;
         Mob = GetComponent<NavMeshAgent>();
     }
-    private void Update()
+    void Update()
     {
-        //float distance = Vector3.Distance(transform.position, Player.transform.position);
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
 
+        //if(distance < MobDistanceRun)
+        //{
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position - dirToPlayer;
+            Mob.SetDestination(newPos);
+        //}
 
-        Vector3 dirToPlayer = transform.position - Player.transform.position;
-        Vector3 newPos = transform.position - dirToPlayer;
+        //Mob.SetDestination(Player.transform.position);
 
-        Mob.SetDestination(newPos);
 
 
 
     }
+
+    //Reference, Code Idea from Brackeys on YouTube
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        healthBar.fillAmount = health / starthealth;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    //Reference, Code Idea from Brackeys on YouTube
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("BING CHILLING you are dead");
+    //
+    //    }
+    //}
 
 }
 
