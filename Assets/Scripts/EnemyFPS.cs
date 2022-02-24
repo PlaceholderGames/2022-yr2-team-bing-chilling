cf51938f866262968123617796a667d1288e2398
scrab333 <72270337+scrab333@users.noreply.github.com>
2022-02-24 16:30:28 +0000

    We have a small moving enemy
    Doesn't do any damage yet
    I hate this
    i hate you
    i am not dying over ukraine

--- Assets/Scripts/EnemyFPS.cs
+++ Assets/Scripts/EnemyFPS.cs
@@ -5,33 +5,28 @@ using UnityEngine.AI;
 
 public class EnemyFPS : MonoBehaviour
 {
-    public float lookRadius = 10f;
-
-    Transform target;
-    NavMeshAgent agent;
-
+    private NavMeshAgent Mob;
+    public GameObject Player;
+    public float MobDistanceRun = 4.4f;
 
     private void Start()
     {
-        target = PlayerManager.instance.player.transform;
-        agent = GetComponent<NavMeshAgent>();
+        Mob = GetComponent<NavMeshAgent>();
     }
-
     private void Update()
     {
-        float distance = Vector3.Distance(target.position, transform.position);
+        //float distance = Vector3.Distance(transform.position, Player.transform.position);
 
-        if (distance <= lookRadius)
-        {
-            agent.SetDestination(target.position);
-        }
+
+        Vector3 dirToPlayer = transform.position - Player.transform.position;
+        Vector3 newPos = transform.position - dirToPlayer;
+
+        Mob.SetDestination(newPos);
+
+
+
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
-    }
 }
 
 
