cc0f6d94a94206f06baca4cb0722a2a9878658f2
scrab333 <72270337+scrab333@users.noreply.github.com>
2022-02-25 14:21:31 +0000

    Buttons are hooked with the script

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseStandartTurrent()
+    {
+        Debug.Log("CRINGE turrent 1 bought");
+    }
+   // public void PurchaseStandartTurrent()
+   // {
+   //     Debug.Log("CRINGE turrent 1 bought");
+   //
+   // }
+   //
+   // public void PurchaseStandartTurrent()
+   // {
+   //     Debug.Log("CRINGE turrent 1 bought");
+   // }
+}
