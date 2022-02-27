9ddd45d011755c7e0aba2658051d43c57282a426
DinoPT <dinarte85@gmail.com>
2022-02-27 10:43:38 +0000

    PauseMenu script

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -9,16 +9,12 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseMenuUI;
 
     // Update is called once per frame
-    void Update()
-    {
-     if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
-            {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (isPaused){
                 Resume();
             }
-            else
-            {
+            else{
                 Pause();
             }
         }   
