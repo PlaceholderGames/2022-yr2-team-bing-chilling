fb65150c5b0dc57b40716e0db533e08eedf87ef6
scrab333 <72270337+scrab333@users.noreply.github.com>
2022-04-01 16:39:53 +0100

    not unlocked levels are now disabled

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class LevelSelect : MonoBehaviour
 {
+
+    public Button[] levelButtons;
+
+    private void Start()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            levelButtons[i].interactable = false;
+        }
+    }
 
     public void Select(string levelName)
     {
